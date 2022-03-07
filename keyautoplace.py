50517acb14e8be3aec4f5903a42895c03f94f4a8
Dennis Lutter <lad1337@gmail.com>
2022-03-07 21:18:17 +0100

    fixes for kicad 5.99 aka 6

--- keyautoplace.py
+++ keyautoplace.py
@@ -9,6 +9,9 @@ import itertools
 import logging
 import re
 
+#                 X    Y
+DIODE_OFFSET = [5.08, 5.03]
+
 
 def PositionInRotatedCoordinates(point, angle):
     """
@@ -47,14 +50,16 @@ def PositionInCartesianCoordinates(point, angle):
     return wxPoint(x, y)
 
 
-class BoardModifier():
+class BoardModifier:
     def __init__(self, logger, board):
         self.logger = logger
         self.board = board
 
     def GetModule(self, reference):
+        self.logger.info("searching...")
         self.logger.info("Searching for {} module".format(reference))
-        module = self.board.FindModuleByReference(reference)
+        # module = self.board.FindModuleByReference(reference)
+        module = self.board.FindFootprintByReference(reference)
         if module == None:
             self.logger.error("Module not found")
             raise Exception("Cannot find module {}".format(reference))
@@ -65,7 +70,9 @@ class BoardModifier():
         module.SetPosition(position)
 
     def SetRelativePositionMM(self, module, referencePoint, direction):
-        position = wxPoint(referencePoint.x + FromMM(direction[0]), referencePoint.y + FromMM(direction[1]))
+        position = wxPoint(
+            referencePoint.x + FromMM(direction[0]), referencePoint.y + FromMM(direction[1])
+        )
         self.SetPosition(module, position)
 
     def AddTrackSegment(self, start, vector, layer=B_Cu):
@@ -96,7 +103,11 @@ class BoardModifier():
         return stop
 
     def Rotate(self, module, rotationReference, angle):
-        self.logger.info("Rotating {} module: rotationReference: {}, rotationAngle: {}".format(module.GetReference(), rotationReference, angle))
+        self.logger.info(
+            "Rotating {} module: rotationReference: {}, rotationAngle: {}".format(
+                module.GetReference(), rotationReference, angle
+            )
+        )
         module.Rotate(rotationReference, angle * -10)
 
 
@@ -137,8 +148,11 @@ class TemplateCopier(BoardModifier):
                 netName = clone.GetNetname()
                 netCode = clone.GetNetCode()
                 netInfoInBoard = self.boardNetsByName[netName]
-                self.logger.info("Cloning track from template: {}:{} -> {}:{}"
-                        .format(netName, netCode, netInfoInBoard.GetNetname(), netInfoInBoard.GetNet()))
+                self.logger.info(
+                    "Cloning track from template: {}:{} -> {}:{}".format(
+                        netName, netCode, netInfoInBoard.GetNetname(), netInfoInBoard.GetNet()
+                    )
+                )
                 clone.SetNet(netInfoInBoard)
                 self.board.Add(clone)
                 track = track.Next()
@@ -159,7 +173,7 @@ class KeyPlacer(BoardModifier):
         # in case of perigoso/keyswitch-kicad-library, stabilizer holes are not part of of switch footprint and needs to be handled
         # separately, check if there is stabilizer with id matching current key and return it
         # stabilizer will be None if not found
-        stabilizer = self.board.FindModuleByReference(stabilizerFormat.format(self.currentKey))
+        stabilizer = self.board.FindFootprintByReference(stabilizerFormat.format(self.currentKey))
         self.currentKey += 1
 
         return key, stabilizer
@@ -172,18 +186,25 @@ class KeyPlacer(BoardModifier):
     def RouteSwitchWithDiode(self, switch, diode, angle):
         self.logger.info("Routing {} with {}".format(switch.GetReference(), diode.GetReference()))
 
-        switchPadPosition = switch.FindPadByName("2").GetPosition()
-        diodePadPosition = diode.FindPadByName("2").GetPosition()
+        switchPadPosition = switch.FindPadByNumber("2").GetPosition()
+        diodePadPosition = diode.FindPadByNumber("2").GetPosition()
 
-        self.logger.debug("switchPadPosition: {}, diodePadPosition: {}".format(switchPadPosition, diodePadPosition))
+        self.logger.debug(
+            "switchPadPosition: {}, diodePadPosition: {}".format(
+                switchPadPosition, diodePadPosition
+            )
+        )
 
         if angle != 0:
             self.logger.info("Routing at {} degree angle".format(angle))
             switchPadPositionR = PositionInRotatedCoordinates(switchPadPosition, angle)
             diodePadPositionR = PositionInRotatedCoordinates(diodePadPosition, angle)
 
-            self.logger.debug("In rotated coordinates: switchPadPosition: {}, diodePadPosition: {}".format(
-                switchPadPositionR, diodePadPositionR))
+            self.logger.debug(
+                "In rotated coordinates: switchPadPosition: {}, diodePadPosition: {}".format(
+                    switchPadPositionR, diodePadPositionR
+                )
+            )
 
             x_diff = abs(diodePadPositionR.x - switchPadPositionR.x)
 
@@ -206,8 +227,13 @@ class KeyPlacer(BoardModifier):
 
             width = key["width"]
             height = key["height"]
-            position = wxPoint((self.keyDistance * key["x"]) + (self.keyDistance * width // 2),
-                (self.keyDistance * key["y"]) + (self.keyDistance * height // 2)) + self.referenceCoordinate
+            position = (
+                wxPoint(
+                    (self.keyDistance * key["x"]) + (self.keyDistance * width // 2),
+                    (self.keyDistance * key["y"]) + (self.keyDistance * height // 2),
+                )
+                + self.referenceCoordinate
+            )
             self.SetPosition(switchModule, position)
 
             if stabilizer:
@@ -219,11 +245,17 @@ class KeyPlacer(BoardModifier):
                     stabilizer.SetOrientationDegrees(90)
 
             diodeModule = self.GetCurrentDiode(diodeFormat)
-            self.SetRelativePositionMM(diodeModule, position, [5.08, 3.03])
+            self.SetRelativePositionMM(diodeModule, position, DIODE_OFFSET)
 
             angle = key["rotation_angle"]
             if angle != 0:
-                rotationReference = wxPoint((self.keyDistance * key["rotation_x"]), (self.keyDistance * key["rotation_y"])) + self.referenceCoordinate
+                rotationReference = (
+                    wxPoint(
+                        (self.keyDistance * key["rotation_x"]),
+                        (self.keyDistance * key["rotation_y"]),
+                    )
+                    + self.referenceCoordinate
+                )
                 self.Rotate(switchModule, rotationReference, angle)
                 if stabilizer:
                     self.Rotate(stabilizer, rotationReference, angle)
@@ -235,21 +267,21 @@ class KeyPlacer(BoardModifier):
                 if diodeModule.GetOrientationDegrees() != 90.0:
                     diodeModule.SetOrientationDegrees(270)
                 if not diodeModule.IsFlipped():
-                    diodeModule.Flip(diodeModule.GetPosition())
+                    diodeModule.Flip(diodeModule.GetPosition(), True)
 
             # append pad:
-            pad = switchModule.FindPadByName("1")
+            pad = switchModule.FindPadByNumber("1")
             net_name = pad.GetNetname()
-            match = re.match(r"^COL(\d+)$", net_name)
+            match = re.match(r"^COL(\d+)$", net_name, re.IGNORECASE)
             if match:
                 column_number = match.groups()[0]
                 column_switch_pads.setdefault(column_number, []).append(pad)
             else:
                 self.logger.warning("Switch pad without recognized net name found.")
             # append diode:
-            pad = diodeModule.FindPadByName("1")
+            pad = diodeModule.FindPadByNumber("1")
             net_name = pad.GetNetname()
-            match = re.match(r"^ROW(\d+)$", net_name)
+            match = re.match(r"^ROW(\d+)$", net_name, re.IGNORECASE)
             if match:
                 row_number = match.groups()[0]
                 row_diode_pads.setdefault(row_number, []).append(pad)
@@ -274,7 +306,9 @@ class KeyPlacer(BoardModifier):
                         x_diff = abs(pos1.x - pos2.x)
                         vector = [0, (y_diff - x_diff)]
                         if vector[1] <= 0:
-                            self.logger.warning("Switch pad to far to route 2 segment track with 45 degree angles")
+                            self.logger.warning(
+                                "Switch pad to far to route 2 segment track with 45 degree angles"
+                            )
                         else:
                             lastPosition = self.AddTrackSegment(pos1, vector, layer=F_Cu)
                             self.AddTrackSegmentByPoints(lastPosition, pos2, layer=F_Cu)
@@ -286,7 +320,9 @@ class KeyPlacer(BoardModifier):
                     if pos1.y == pos2.y:
                         self.AddTrackSegmentByPoints(pos1, pos2)
                     else:
-                        self.logger.warning("Automatic diode routing supported only when diodes aligned vertically")
+                        self.logger.warning(
+                            "Automatic diode routing supported only when diodes aligned vertically"
+                        )
 
 
 class KeyAutoPlaceDialog(wx.Dialog):
@@ -296,60 +332,60 @@ class KeyAutoPlaceDialog(wx.Dialog):
         row1 = wx.BoxSizer(wx.HORIZONTAL)
 
         text = wx.StaticText(self, -1, "Select kle json file:")
-        row1.Add(text, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        row1.Add(text, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
 
         layoutFilePicker = wx.FilePickerCtrl(self, -1)
-        row1.Add(layoutFilePicker, 1, wx.EXPAND|wx.ALL, 5)
+        row1.Add(layoutFilePicker, 1, wx.EXPAND | wx.ALL, 5)
 
         row2 = wx.BoxSizer(wx.HORIZONTAL)
 
         keyAnnotationLabel = wx.StaticText(self, -1, "Key annotation format string:")
-        row2.Add(keyAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        row2.Add(keyAnnotationLabel, 1, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
 
-        keyAnnotationFormat = wx.TextCtrl(self, value='SW{}')
-        row2.Add(keyAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
+        keyAnnotationFormat = wx.TextCtrl(self, value="SW{}")
+        row2.Add(keyAnnotationFormat, 1, wx.EXPAND | wx.ALL, 5)
 
         row3 = wx.BoxSizer(wx.HORIZONTAL)
 
-        stabilizerAnnotationLabel = wx.StaticText(self, -1, "Key annotation format string:")
-        row3.Add(stabilizerAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        stabilizerAnnotationLabel = wx.StaticText(self, -1, "Stab annotation format string:")
+        row3.Add(stabilizerAnnotationLabel, 1, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
 
-        stabilizerAnnotationFormat = wx.TextCtrl(self, value='ST{}')
-        row3.Add(stabilizerAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
+        stabilizerAnnotationFormat = wx.TextCtrl(self, value="ST{}")
+        row3.Add(stabilizerAnnotationFormat, 1, wx.EXPAND | wx.ALL, 5)
 
         row4 = wx.BoxSizer(wx.HORIZONTAL)
 
         diodeAnnotationLabel = wx.StaticText(self, -1, "Diode annotation format string:")
-        row4.Add(diodeAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        row4.Add(diodeAnnotationLabel, 1, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
 
-        diodeAnnotationFormat = wx.TextCtrl(self, value='D{}')
-        row4.Add(diodeAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
+        diodeAnnotationFormat = wx.TextCtrl(self, value="D{}")
+        row4.Add(diodeAnnotationFormat, 1, wx.EXPAND | wx.ALL, 5)
 
         row5 = wx.BoxSizer(wx.HORIZONTAL)
 
         tracksCheckbox = wx.CheckBox(self, label="Add tracks")
         tracksCheckbox.SetValue(True)
-        row5.Add(tracksCheckbox, 1, wx.EXPAND|wx.ALL, 5)
+        row5.Add(tracksCheckbox, 1, wx.EXPAND | wx.ALL, 5)
 
         row6 = wx.BoxSizer(wx.HORIZONTAL)
 
         text = wx.StaticText(self, -1, "Select controler circuit template:")
-        row6.Add(text, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        row6.Add(text, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
 
         templateFilePicker = wx.FilePickerCtrl(self, -1)
-        row6.Add(templateFilePicker, 1, wx.EXPAND|wx.ALL, 5)
+        row6.Add(templateFilePicker, 1, wx.EXPAND | wx.ALL, 5)
 
         box = wx.BoxSizer(wx.VERTICAL)
 
-        box.Add(row1, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row2, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row3, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row4, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row5, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row6, 0, wx.EXPAND|wx.ALL, 5)
+        box.Add(row1, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(row2, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(row3, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(row4, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(row5, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(row6, 0, wx.EXPAND | wx.ALL, 5)
 
-        buttons = self.CreateButtonSizer(wx.OK|wx.CANCEL)
-        box.Add(buttons, 0, wx.EXPAND|wx.ALL, 5)
+        buttons = self.CreateButtonSizer(wx.OK | wx.CANCEL)
+        box.Add(buttons, 0, wx.EXPAND | wx.ALL, 5)
 
         self.SetSizerAndFit(box)
         self.layoutFilePicker = layoutFilePicker
@@ -395,34 +431,44 @@ class KeyAutoPlace(ActionPlugin):
             logging.root.removeHandler(handler)
 
         # set up logger
-        logging.basicConfig(level=logging.DEBUG,
-                            filename="keyautoplace.log",
-                            filemode='w',
-                            format='%(asctime)s %(name)s %(lineno)d: %(message)s',
-                            datefmt='%H:%M:%S')
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename="keyautoplace.log",
+            filemode="w",
+            format="%(asctime)s %(name)s %(lineno)d: %(message)s",
+            datefmt="%H:%M:%S",
+        )
         self.logger = logging.getLogger(__name__)
         self.logger.info("Plugin executed with python version: " + repr(sys.version))
 
     def Run(self):
         self.Initialize()
 
-        pcbFrame = [x for x in wx.GetTopLevelWindows() if x.GetName() == 'PcbFrame'][0]
+        pcbFrame = [x for x in wx.GetTopLevelWindows() if x.GetName() == "PcbFrame"][0]
 
-        dlg = KeyAutoPlaceDialog(pcbFrame, 'Title', 'Caption')
+        dlg = KeyAutoPlaceDialog(pcbFrame, "Title", "Caption")
         if dlg.ShowModal() == wx.ID_OK:
             templatePath = dlg.GetTemplatePath()
             if templatePath:
-                templateCopier = TemplateCopier(self.logger, self.board, templatePath, dlg.IsTracks())
+                templateCopier = TemplateCopier(
+                    self.logger, self.board, templatePath, dlg.IsTracks()
+                )
                 templateCopier.Run()
 
             layoutPath = dlg.GetLayoutPath()
             if layoutPath:
-                with open(layoutPath, "r") as f:
+                with open(layoutPath, "r", encoding="utf-8") as f:
                     textInput = f.read()
                 layout = json.loads(textInput)
-                self.logger.info("User layout: {}".format(layout))
+                # self.logger.info("User layout: {}".format(layout))
+                self.logger.info("Loaded the layout")
                 placer = KeyPlacer(self.logger, self.board, layout)
-                placer.Run(dlg.GetKeyAnnotationFormat(), dlg.GetStabilizerAnnotationFormat(), dlg.GetDiodeAnnotationFormat(), dlg.IsTracks())
+                placer.Run(
+                    dlg.GetKeyAnnotationFormat(),
+                    dlg.GetStabilizerAnnotationFormat(),
+                    dlg.GetDiodeAnnotationFormat(),
+                    dlg.IsTracks(),
+                )
 
         dlg.Destroy()
         logging.shutdown()
@@ -430,10 +476,10 @@ class KeyAutoPlace(ActionPlugin):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Keyboard's key autoplacer")
-    parser.add_argument('-l', '--layout', required=True, help="json layout definition file")
-    parser.add_argument('-b', '--board', required=True, help=".kicad_pcb file to be processed")
-    parser.add_argument('-r', '--route', action="store_true", help="Enable experimental routing")
-    parser.add_argument('-t', '--template', help="controler circuit template")
+    parser.add_argument("-l", "--layout", required=True, help="json layout definition file")
+    parser.add_argument("-b", "--board", required=True, help=".kicad_pcb file to be processed")
+    parser.add_argument("-r", "--route", action="store_true", help="Enable experimental routing")
+    parser.add_argument("-t", "--template", help="controler circuit template")
 
     args = parser.parse_args()
     layoutPath = args.layout
@@ -442,9 +488,7 @@ if __name__ == "__main__":
     templatePath = args.template
 
     # set up logger
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s: %(message)s',
-                        datefmt='%H:%M:%S')
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s: %(message)s", datefmt="%H:%M:%S")
     logger = logging.getLogger(__name__)
 
     board = LoadBoard(boardPath)
